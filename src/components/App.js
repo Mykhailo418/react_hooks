8f051fa4b9ca0deeba681010721e7ee2c2f033a0
Mykhailo418 <smmichael418@gmail.com>
2019-06-01 18:49:57 +0300

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component{
+    static propTypes = {}
+
+    render(){
+      return (
+        <h1>React Hooks</h1>
+      );
+    }
+}
+
+export default App;
